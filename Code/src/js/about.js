498c0e4d246ae68bd919661ab7eedaa89192d1fa
akshayreddynalla <akshaykumarreddyn@usf.edu>
2023-10-29 19:34:42 -0400

    Added About_Page

---
+++ Code/src/js/about.js
@@ -0,0 +1,10 @@
+
+function ImageClick(){
+    const data = document.getElementById("all_profiles_one");
+    if(data.style.display == "none"){
+        data.style.display == "block";
+    }
+    else{
+        data.style.display = "none";
+    }
+}
